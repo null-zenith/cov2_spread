46c9b50ac32aacc898ba450b1e2fd9441d96215c
Andrei Samoilov <andrei.samoilov@gmail.com>
2026-08-24 17:53:31 +0300

    Add files via upload

---
+++ cov2plus1_cmd.R
@@ -0,0 +1,107 @@
+suppressMessages(library(magrittr))
+suppressMessages(library(optparse))
+suppressMessages(library(dplyr))
+options(warn=1)
+
+
+
+option_list = list(
+  make_option("--nc_path", action="store", default=NA, type='character',
+              help="Path to the Nextclade table(s), comma-separated"),
+  make_option("--skip", action="store", default=FALSE, type='logical',
+              help="If TRUE, skip to plus1 part"))
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+unss <- function(x, split = ',', fix = FALSE){
+  splt <- strsplit(x, split = split)
+  lst_lngth <- lapply(splt, length) %>% unlist
+  if(fix){if(sum(lst_lngth == 0)) splt[lst_lngth == 0] <- ''}
+  
+  res <- unlist(splt)
+  return(res)
+}
+
+
+if(!opt$skip){
+
+  print('Filtering...')
+  nc_tbl <- read.table(opt$nc_path, sep = '\t', header = TRUE, quote = '', comment.char = '')
+  nc_tbl <- nc_tbl[(nc_tbl$totalMissing <= 300 & nc_tbl$qc.overallStatus == 'good' & (nc_tbl$alignmentEnd - nc_tbl$alignmentStart > 29000)), c('seqName', 'Nextclade_pango', 'partiallyAliased', 'substitutions')]
+  write.table(nc_tbl, gsub('tsv', 'Nfiltered.tsv', opt$nc_path), sep = '\t', row.names = FALSE, col.names = TRUE, quote = FALSE)
+  
+  nc_tbl <- nc_tbl[, c('Nextclade_pango', 'partiallyAliased', 'substitutions')]
+  
+  print('Finding discordant subs and lineages... Just FYI, this does not affect the filtration!')
+  nc_tbl_sub_diff_pango <-  nc_tbl %>% group_by(substitutions) %>% filter(n_distinct(partiallyAliased) > 1) %>% distinct(substitutions, partiallyAliased) %>% ungroup()
+  nc_tbl_sub_diff_pango <- nc_tbl_sub_diff_pango[order(nc_tbl_sub_diff_pango$partiallyAliased), ]
+  write.table(nc_tbl_sub_diff_pango[, c("partiallyAliased", "substitutions")], gsub('tsv', 'diff_pango.tsv', opt$nc_path), sep = '\t', row.names = FALSE, col.names = TRUE, quote = FALSE)
+  
+  
+  
+  nc_tbl <- unique(nc_tbl)
+  
+  write.table(nc_tbl, gsub('tsv', 'short.tsv', opt$nc_path), sep = '\t', row.names = FALSE, col.names = TRUE, quote = FALSE)
+  
+  
+  nsub <- sapply(nc_tbl$substitutions, function(x) length(unss(x)))
+  nsub_list <- sort(unique(nsub))
+  print(paste0('number of mutations up to ', max(nsub)))
+  
+  system(paste0('mkdir ', dirname(opt$nc_path), '/by_nsub/'))
+  for(i in nsub_list){
+    print(i)
+    write.table(nc_tbl[nsub == i, ], paste0(dirname(opt$nc_path), '/by_nsub/', i, '.tsv'), sep = '\t', row.names = FALSE, quote = FALSE)
+  }
+}
+
+
+
+print('Finding plus one ...')
+CON <- file(paste0(dirname(opt$nc_path), '/plus1.txt'), 'a')
+print(CON)
+fls <- system(paste0('ls ', dirname(opt$nc_path), '/by_nsub/*.tsv'), intern = TRUE)
+
+nsub <- as.numeric(gsub('.tsv', '', basename(fls)))
+
+for(i in 0:max(nsub)){
+  print(paste0('Processing file with ', i, ' mutations...'))
+  # print(paste0(dirname(opt$nc_path), '/by_nsub/', i, '.tsv'))
+  if (file.exists(paste0(dirname(opt$nc_path), '/by_nsub/', i, '.tsv')) & file.exists(paste0(dirname(opt$nc_path), '/by_nsub/', i+1, '.tsv'))){
+    tbl1 <- read.table(paste0(dirname(opt$nc_path), '/by_nsub/', i, '.tsv'), sep = '\t', header = TRUE)
+    tbl1[is.na(tbl1)] <- ''
+    uniq_ptrns1 <- tbl1$substitutions %>% unique
+
+    tbl2 <- read.table(paste0(dirname(opt$nc_path), '/by_nsub/', i+1, '.tsv'), sep = '\t', header = TRUE)
+    uniq_ptrns2 <- tbl2$substitutions %>% unique
+
+    if(i == 0){
+      write(paste0('root -> ', paste0(uniq_ptrns2, collapse = ';')), CON)
+    } else {
+      uniq_subs2 <- unss(uniq_ptrns2, split = ',') %>% unique
+      rev_index <- sapply(uniq_subs2, function(x) uniq_ptrns2[grepl(x, uniq_ptrns2)]) %>% as.list
+
+      # print(uniq_ptrns1)
+      for(j in 1:length(uniq_ptrns1)){
+
+        current_muts <- unss(uniq_ptrns1[j])
+
+        for(k in 1:length(current_muts)){
+          if (k == 1){
+            ptrns_tmp <- rev_index[[current_muts[k]]]
+          } else {
+            ptrns_tmp <- ptrns_tmp[ptrns_tmp %in% rev_index[[current_muts[k]]]]
+            if(length(ptrns_tmp) == 0) break
+          }
+
+
+        }
+        if(length(ptrns_tmp) != 0) write(paste0(uniq_ptrns1[j], ' -> ', paste0(ptrns_tmp, collapse = ';')), CON) # print(paste0(uniq_ptrns1[j], ' -> ', paste0(ptrns_tmp, collapse = ';'))) 
+
+      }
+      # print(ptrns_tmp)
+
+    }
+  }
+}
+close(CON)
